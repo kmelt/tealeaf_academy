8a902f32e908b445d79a851395632f0a6f51aa55
K Melton <kay.layne@gmail.com>
2015-04-17 20:53:47 -0500

    finished basics chapter

---
+++ intro_to_programming/basics_exercises.rb
@@ -0,0 +1,56 @@
+# Exercise 1
+
+puts "Kayley " + "Melton"
+
+# Exercise 2
+
+puts 4238
+puts "Thousands"
+puts (4238 / 1000)
+puts "Hundreds"
+puts (4238 % 1000 / 100)
+puts "Tens"
+puts (4238 % 1000 % 100 / 10)
+puts "Ones"
+puts (4238 % 1000 % 100 % 10)
+
+# Exercise 3
+
+movies = { jaws: 1975,
+					 anchorman: 2004,
+					 man_of_steel: 2013,
+					 a_beautiful_mind: 2001,
+					 the_evil_dead: 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+# Exercise 4
+
+movie_dates = [1975, 2004, 2013, 2001, 1981]
+
+puts movie_dates[0]
+puts movie_dates[1]
+puts movie_dates[2]
+puts movie_dates[3]
+puts movie_dates[4]
+
+# Exercise 5
+
+puts 1*2*3*4*5
+puts 1*2*3*4*5*6
+puts 1*2*3*4*5*6*7
+puts 1*2*3*4*5*6*7*8
+
+# Exercise 6
+
+puts 7.23 * 7.23
+puts 23.287 * 23.287
+puts 4.238 * 4.238
+
+# Exercise 7
+
+puts "Used ) to close instead of } in code."
